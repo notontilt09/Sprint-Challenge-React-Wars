48f07f91da34b8fa1de15c5772c27555eadc99a1
Daniel Weinman <namniewd@gmail.com>
2019-01-11 11:40:13 -0500

    added some styles to the list items

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,8 +4,8 @@ import './StarWars.css';
 const Character = props => {
     return (
         <div className="character-item">
-            <h1>{props.character.name}</h1>
-            <ul>
+            <h1 className="character-name">{props.character.name}</h1>
+            <ul className="details">
                 <li>Birth Year: {props.character.birth_year}</li>
                 <li>Gender: {props.character.gender}</li>
             </ul>
